f343943d1eab7f11d67a174f3aab45d01634fd7c
Kevin McCarthy <kevinpatrickmccarthy@gmail.com>
2019-08-24 06:46:26 +0100

    anotherchange

---
+++ mergeWhenReady.js
@@ -0,0 +1,5 @@
+// click first merge button
+document.querySelector("#partial-pull-merging > div.merge-pr.js-merge-pr.js-details-container.Details.is-merging > div > div > div > div.merge-message > div.select-menu.d-inline-block > div > button.btn-group-merge.border-right-0.rounded-left-1.btn.btn-primary.BtnGroup-item.js-details-target").click()
+
+// click confirm merge button
+document.querySelector("#partial-pull-merging > div.merge-pr.js-merge-pr.js-details-container.Details.is-merging.js-transitionable.open.Details--on > form > div > div.commit-form-actions > div > div.BtnGroup.btn-group-merge > button").click()
